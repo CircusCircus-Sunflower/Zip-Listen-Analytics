3190bfd51b682f99a9623d6a610c5269733901cf
imiles <ieshia.miles@gmail.com>
2026-01-13 15:48:03 -0500

    "Update database Url"

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://zipuser:zippassword@db:5432/ziplistendb"
+    "postgresql://sunflower_user:zipmusic@xo.zipcode.rocks:9088/sunflower"
 )
 
 engine = create_engine(DATABASE_URL)
